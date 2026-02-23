3b14711c69067490869ce32410d35fdef5a240c3
Tushya-web <tushyparmar@gmail.com>
2026-02-24 02:06:12 +0530

    new database

--- samanloop/settings.py
+++ samanloop/settings.py
@@ -87,13 +87,14 @@ WSGI_APPLICATION = 'samanloop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
